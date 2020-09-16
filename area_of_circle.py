4166e863eca190a621b4d01309d4fc501ee98b9e
vani22badkul <43854197+vani22badkul@users.noreply.github.com>
2020-09-16 11:14:32 +0530

    Add files via upload

---
+++ area_of_circle.py
@@ -0,0 +1,2 @@
+r=eval(input("input the radius of circle :" ))
+print("the radius of circle with radius ",r," is:",2*3.14*r)
---
+++ file_name.py
@@ -0,0 +1,3 @@
+n=input('Input the filename:')
+if '.py' in n:
+    print('The extension of the file is : python')
