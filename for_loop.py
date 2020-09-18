821536a778b14a139efb17f66a86d247bdc84f8a
vani22badkul <43854197+vani22badkul@users.noreply.github.com>
2020-09-18 11:28:21 +0530

    created for_loop.py
    it prints the range of positive numbers from a given list.

---
+++ for_loop.py
@@ -0,0 +1,16 @@
+
+list1=[12,-7,5,64,-14]
+print('Input:list1 ',list1)
+for i in list1:
+    if i>=0:
+        print(i,end=' ')
+
+print('\n')
+        
+list2=[12,14,-95,3]
+print('Input:list1 ',list2)
+for i in list2:
+    if i>=0:
+        print(i,end=' ')
+
+    
